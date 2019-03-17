a8ff8dec3f995c139be00c92486804c770e725ac
niro_ <nirokal92@gmail.com>
2019-03-17 05:07:05 -0400

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.niro_.typingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int score=0;
+
+        String [] words = {"Green", "Blue", "Dragon", "Eclipse", "Prince", "Help", "World", "Planets"};
+
+    }
+}
